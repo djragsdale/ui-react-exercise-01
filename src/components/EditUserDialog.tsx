3973fa0e7366587884e6a2e8db75f7cf8a7d7536
DrHyde111 <pawel.j.skalski@gmail.com>
2023-03-24 17:50:10 +0100

    Implemented EditUserDialog

---
+++ src/components/EditUserDialog.tsx
@@ -0,0 +1,64 @@
+import {
+  Button,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  HTMLSelect,
+} from "@blueprintjs/core";
+import { useState } from "react";
+import { User } from "../types/User";
+
+type EditUserDialogProps = {
+  isOpen: boolean;
+  user: User;
+  onDialogSubmit: (user: User) => void;
+  onDialogCancel: () => void;
+};
+const roles: string[] = ["Engineer", "Administrator", "Support Technician"];
+export const EditUserDialog: React.FC<EditUserDialogProps> = ({
+  isOpen,
+  user,
+  onDialogCancel,
+  onDialogSubmit,
+}) => {
+  const [newRole, setNewRole] = useState(user.role);
+
+  const handleDialogSubmit = () => {
+    onDialogSubmit({ ...user, role: newRole });
+  };
+  const dialogFooterActions = (
+    <>
+      <Button
+        id="edit-dialog-cancel-button"
+        intent="danger"
+        text="Cancel"
+        onClick={onDialogCancel}
+      />
+      <Button
+        id="edit-dialog-submit-button"
+        intent="success"
+        text="Confirm"
+        onClick={() => handleDialogSubmit()}
+      />
+    </>
+  );
+
+  return (
+    <Dialog isOpen={isOpen} title="Informational dialog" icon="info-sign">
+      <DialogBody>
+        <p>
+          Change role for user: {user.idUser} {user.profile.firstName}
+          {user.profile.lastName}
+        </p>
+        <HTMLSelect onChange={(event) => setNewRole(event.target.value)}>
+          {roles.map((role) => (
+            <option selected={role === newRole} value={role}>
+              {role}
+            </option>
+          ))}
+        </HTMLSelect>
+      </DialogBody>
+      <DialogFooter actions={dialogFooterActions} />
+    </Dialog>
+  );
+};
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,14 +1,19 @@
 import { Spinner } from "@blueprintjs/core";
+import { useRef, useState } from "react";
 
 import type { User } from "../types/User";
+import { EditUserDialog } from "./EditUserDialog";
 
 type UserListProps = {
   isLoading?: boolean;
-  onEditUser?: (user: User) => void;
+  onEditUser?: (idUser: number, user: User) => void;
   users?: User[];
 };
 
 export const UserList = ({ isLoading, onEditUser, users }: UserListProps) => {
+  const [isEditDialogOpen, setIsEditDialogOpen] = useState<boolean>(false);
+  const choosedUser = useRef<User | null>();
+
   if (isLoading) {
     return <Spinner intent="primary" size={100} />;
   }
@@ -17,32 +22,56 @@ export const UserList = ({ isLoading, onEditUser, users }: UserListProps) => {
     return <p>There are no users to be displayed.</p>;
   }
 
+  const handleEditRoleButtonClick = (user: User) => {
+    choosedUser.current = user;
+    setIsEditDialogOpen(true);
+  };
+
+  const handleEditUser = (user: User) => {
+    if (onEditUser) {
+      onEditUser(user.idUser, user);
+      setIsEditDialogOpen(false);
+    }
+  };
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>First name</th>
-          <th>Last name</th>
-          <th>Role</th>
-          {onEditUser && <th>Actions</th>}
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user) => (
-          <tr key={user.idUser + user.profile.firstName}>
-            <td>{user.idUser}</td>
-            <td>{user.profile.firstName}</td>
-            <td>{user.profile.lastName}</td>
-            <td>{user.role}</td>
-            {onEditUser && (
-              <td>
-                <button onClick={() => onEditUser(user)}>Edit role</button>
-              </td>
-            )}
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Role</th>
+            {onEditUser && <th>Actions</th>}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.idUser + user.profile.firstName}>
+              <td>{user.idUser}</td>
+              <td>{user.profile.firstName}</td>
+              <td>{user.profile.lastName}</td>
+              <td>{user.role}</td>
+              {onEditUser && (
+                <td>
+                  <button onClick={() => handleEditRoleButtonClick(user)}>
+                    Edit role
+                  </button>
+                </td>
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {choosedUser.current && onEditUser && (
+        <EditUserDialog
+          isOpen={isEditDialogOpen}
+          user={choosedUser.current}
+          onDialogSubmit={handleEditUser}
+          onDialogCancel={() => setIsEditDialogOpen(false)}
+        />
+      )}
+    </>
   );
 };
