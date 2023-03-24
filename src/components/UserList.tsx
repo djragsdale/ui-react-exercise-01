2b9584733f04fc7bbefd3ecc2f284fcd8ed64d66
DrHyde111 <pawel.j.skalski@gmail.com>
2023-03-24 16:32:34 +0100

    Implemented user list component

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -13,5 +13,36 @@ export const UserList = ({ isLoading, onEditUser, users }: UserListProps) => {
     return <Spinner intent="primary" size={100} />;
   }
 
-  return <>Build user list here</>;
+  if (!users) {
+    return <p>There are no users to be displayed.</p>;
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>First name</th>
+          <th>Last name</th>
+          <th>Role</th>
+          {onEditUser && <th>Actions</th>}
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.idUser + user.profile.firstName}>
+            <td>{user.idUser}</td>
+            <td>{user.profile.firstName}</td>
+            <td>{user.profile.lastName}</td>
+            <td>{user.role}</td>
+            {onEditUser && (
+              <td>
+                <button onClick={() => onEditUser(user)}>Edit role</button>
+              </td>
+            )}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
